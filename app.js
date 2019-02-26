f3c97f14f48b1a6ed7b54803bced92c8920d41d5
patrykbaklaj@gmail.com <patrykbaklaj@gmail.com>
2019-02-26 22:15:53 +0100

    Add new routes - login, register, secrets

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.get('/secrets', (req, res) => {
+    res.render('secrets');
+});
+
 
 
 
